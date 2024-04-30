45d0cf16fbf4c89e8e0db2c27df5d579ac7abacc
Infinite Programmer <imiibrahim7860@gmail.com>
2024-04-30 23:36:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("calculateButton").addEventListener("click", function() {
+    var width = parseFloat(document.getElementById("width").value);
+    var thickness = parseFloat(document.getElementById("thickness").value);
+    var length = parseFloat(document.getElementById("length").value);
+    var quantity = parseFloat(document.getElementById("quantity").value);
+
+    var widthFeet = width / 12;
+    var thicknessFeet = thickness / 12;
+    var lengthFeet = length / 12;
+
+    var volumeOneUnit = widthFeet * thicknessFeet * lengthFeet;
+
+    var totalVolume = volumeOneUnit * quantity;
+
+    // Display result
+    document.getElementById("result").innerHTML = "CFT: " + totalVolume.toFixed(2) + " CFT";
+});
